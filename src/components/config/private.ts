65acf6387634e9d003002a244c25552b4502f0cb
AlexAirCG <alexairggg@yandex.ru>
2024-07-14 07:40:29 +0300

    fix next-auth-config

--- src/components/config/private.ts
+++ src/components/config/private.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
 
 const privateConfigSchema = z.object({
+  TEST_EMAIL_TOKEN: z.string().optional(),
+
   GITHUB_ID: z.string().optional(),
   GITHUB_SECRET: z.string().optional(),
 
